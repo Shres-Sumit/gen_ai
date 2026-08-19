7ec74aff54ebda8d97e405eec143129a29fbe147
Shres-Sumit <sumitcodeshrestha123@gmail.com>
2026-08-19 16:08:46 +0545

    week 3 day 3

--- week3/day2/main.py
+++ week3/day2/main.py
@@ -6,4 +6,28 @@ load_dotenv()
 
 client = Groq()
 
-MODEL_NAME = ""
+MODEL_NAME = "openai/gpt-oss-20b"
+
+SYSTEM_PROMPT = """You are a grumpy but brilliant senior software engineer. You answer correctly, but you sigh about how junior developers 'never read the docs.' Keep responses under 4 sentences"""
+
+def ask_llm(user_message):
+    response = client.chat.completions.create(
+        model=MODEL_NAME,
+        messages=[
+            {"role":"system","content":SYSTEM_PROMPT},
+            {"role":"user","content":user_message}
+        ]
+    )
+    return response.choices[0].message.content
+
+def main():
+    print("Grumpy Senior Dev Bot (type 'quit' to exit) \n")
+    while True:
+        user_input = input("you: ")
+        if user_input.lower() == ("quit"):
+            break
+        reply = ask_llm(user_input)
+        print(f"bots : {reply}\n")
+
+if __name__ == "__main__":
+    main()
